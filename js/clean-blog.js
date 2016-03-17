872cccb16bc965f4ebaeb00df250c319cb4836b8
gandjustas <gandjustas@mail.ru>
2016-03-17 23:40:08 +0300

    Added post

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -17,33 +17,22 @@ $(document).ready(function () {
 	$('iframe[src*="vimeo.com"]').addClass('embed-responsive-item');
 });
 
-// Floating label headings for the contact form
-$(function() {
-    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-    }).on("focus", ".floating-label-form-group", function() {
-        $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function() {
-        $(this).removeClass("floating-label-form-group-with-focus");
-    });
-});
-
 // Navigation Scripts to Show Header on Scroll-Up
-jQuery(document).ready(function($) {
+$(function() {
     var MQL = 1170;
+    var headerHeight = $('.intro-header').height();
 
     //primary navigation slide-in effect
     if ($(window).width() > MQL) {
-        var headerHeight = $('.navbar-custom').height();
         $(window).on('scroll', {
                 previousTop: 0
             },
             function() {
                 var currentTop = $(window).scrollTop();
                 //check if user is scrolling up
-                if (currentTop < this.previousTop) {
+                if (currentTop < this.previousTop ) {
                     //if scrolling up...
-                    if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
+                    if (currentTop > headerHeight && $('.navbar-custom').hasClass('is-fixed')) {
                         $('.navbar-custom').addClass('is-visible');
                     } else {
                         $('.navbar-custom').removeClass('is-visible is-fixed');
